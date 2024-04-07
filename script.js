1cfbbffc0351d725b0dc97b2dac042805c6ea23f
Berkan K <berkan0hk@gmail.com>
2024-04-07 08:49:23 +0300

    color is done

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const sketchScreen = document.querySelector(".main-container");
 
-for (let i = 1; i < 257; i++) {
+for (let i = 0; i < 267; i++) {
   const divs = document.createElement('div');
   sketchScreen.appendChild(divs);
   divs.style.border = "1px solid black";
-  divs.style.height = "25px";
-  divs.style.width = "25px";
+  divs.style.height = "16px";
+  divs.style.width = "16px";
+  divs.addEventListener('mouseover', () => {
+    divs.style.backgroundColor = 'blue';
+  })
 }
+
 
 
