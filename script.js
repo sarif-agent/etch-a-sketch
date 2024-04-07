825d887d4290d43be3dfe06c6b55d34f0265a4ca
Berkan K <berkan0hk@gmail.com>
2024-04-08 01:40:07 +0300

    finished

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const container = document.querySelector(".container");
-
 const resetButton = document.querySelector("button");
 
-sizeOfGrid = 16;
+// sizeOfGrid = 16;
 
 const createGrid = (amtOfGrids) => {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+
   for (let i = 0; i < amtOfGrids; i++) {
     const row = document.createElement('div');
     row.classList.add('grid-row');
 
     for (let j = 0; j < amtOfGrids; j++) {
-      const widthAndHeight = 960 / sizeOfGrid
+      const widthAndHeight = 960 / amtOfGrids
       const gridBox = document.createElement('div');
       gridBox.classList.add('grid-box');
       gridBox.style.width = `${widthAndHeight}px`;
@@ -24,24 +26,29 @@ const createGrid = (amtOfGrids) => {
 
     }
 
-    container.appendChild(row);
+    wrapper.appendChild(row);
   }
+  container.appendChild(wrapper);
 }
-
-console.log(resetButton);
 
 resetButton.addEventListener('click', () => {
   let userSize = Number(prompt('What dimensions do you want for the new grid?'))
 
-
-
   while (userSize > 100) {
     userSize = Number(prompt('Pick a smaller number than 100'))
-    createGrid(userSize);
+  }
+
+  const wrapper = document.querySelector('.wrapper');
+
+  if (!wrapper) {
+    createGrid(userSize)
+  } else {
+    wrapper.remove()
+    createGrid(userSize)
   }
 })
 
-createGrid(sizeOfGrid)
+
 
 
 
