64d65aa79748979fa729302bcf0cc115d49f7683
Berkan K <berkan0hk@gmail.com>
2024-04-07 10:41:00 +0300

    code replacement and build from zero

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const sketchScreen = document.querySelector(".main-container");
+const container = document.querySelector(".container");
 
-for (let i = 0; i < 267; i++) {
-  const divs = document.createElement('div');
-  sketchScreen.appendChild(divs);
-  divs.style.border = "1px solid black";
-  divs.style.height = "16px";
-  divs.style.width = "16px";
-  divs.addEventListener('mouseover', () => {
-    divs.style.backgroundColor = 'blue';
-  })
+const startButton = document.querySelector(".start");
+
+
+const createGrid = (amtOfGrids) => {
+  for (let i = 0; i < amtOfGrids; i++) {
+    const row = document.createElement('div');
+    row.classList.add('grid-row');
+
+    for (let j = 0; j < amtOfGrids; j++) {
+      const gridBox = document.createElement('div');
+      gridBox.classList.add('grid-box');
+      row.appendChild(gridBox)
+
+      gridBox.addEventListener('mouseenter', () => {
+        gridBox.style.backgroundColor = 'black';
+      })
+
+    }
+
+    container.appendChild(row);
+  }
 }
+
+createGrid(5)
 
 
 
